5fa0d50c2e1ad5795a55c8ca75ff0b86e7f27aab
Luciano <lucho.2012.tandil@gmail.com>
2024-10-13 02:59:58 -0300

    endpoints de consulta de puntos y guardado de reclamos funcional

---
+++ definicion_inicial_db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `reclamos` 
+    (`id` VARCHAR(36) NOT NULL , 
+    `titulo` VARCHAR(255) NOT NULL , 
+    `detalles` VARCHAR(1024) NOT NULL , 
+    `imagenes` JSON NOT NULL , 
+    `ipv4` VARCHAR(255) NOT NULL,
+    `ipv6` VARCHAR(255) NOT NULL, 
+    `ts` DATETIME NOT NULL, 
+    `user_agent` VARCHAR(255) NOT NULL,
+    `posicion` VARCHAR(255) NOT NULL
+PRIMARY KEY (`id`)) ENGINE = InnoDB; 
+
+CREATE TABLE `reportes` 
+    (`id` VARCHAR(36) NOT NULL, 
+    `detalle` VARCHAR(255) NOT NULL, 
+    `ts` DATETIME NOT NULL, 
+    `ipv4` VARCHAR(255) NOT NULL, 
+    `ipv6` VARCHAR(255) NOT NULL, 
+    `user_agent` VARCHAR(255) NOT NULL, 
+    `reclamo_id` VARCHAR(36) NOT NULL, 
+PRIMARY KEY (`id`(36))) ENGINE = InnoDB; 
